1deb68edce97d07f51aeba126fe06c5e4e80bbe5
LouieT <Louis@bienabee.com>
2014-02-17 22:18:03 -0800

    Added DBIDup C# Files
    Working on the C# version of DBID up... GUI is in shambles. Working out
    the logic in C# first.

---
+++ Utilities/DBIDup/DBIDupV1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DBID up v 1.0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(459, 325);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Index Of";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 51);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(379, 168);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(459, 296);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Read DBID";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(459, 247);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 43);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Read Replace";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 383);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Utilities/DBIDup/DBIDupV1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            String file_name = "\\Users\\Admin\\Desktop\\DBIDup\\dbid.xml";
+
+           
+
+            int result = file_name.IndexOf("dbid");
+
+            MessageBox.Show("You found the dbid" + result.ToString());
+
+
+
+       
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            String file_name = "\\Users\\Admin\\Desktop\\DBIDup\\dbid.xml";
+
+               System.IO.StreamReader objReader;
+                objReader = new System.IO.StreamReader(file_name);
+
+            //String file_contents = objReader.ReadToEnd();
+
+                textBox1.Text = objReader.ReadToEnd();
+                objReader.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            char[] startChar = { '<' };
+
+            string findConStr = "<DB_CONNSTR_FORMAT>";
+            string closeConStr = ("</" + findConStr.TrimStart(startChar));
+            int conLen = (findConStr.Length);
+            MessageBox.Show(conLen.ToString());
+
+            string findDB = "<DBSERVER_NAME>";
+            string findDir = "<PHYSICAL_DIRECTORY>";
+            string findPW = "<DB_USER_PASS>";
+            
+            string dbidFile = "\\Users\\Admin\\Desktop\\DBIDup\\dbid.xml";
+            
+            string text = File.ReadAllText(dbidFile);
+
+            MessageBox.Show(text);
+
+            int startConStr = text.IndexOf(findConStr)+(conLen);
+            int endConStr = text.IndexOf(closeConStr);
+
+            MessageBox.Show(startConStr.ToString());
+            MessageBox.Show(endConStr.ToString());
+
+            int oConStrLen = (endConStr - startConStr);
+
+            string oConStr = text.Substring(startConStr, oConStrLen);
+
+            MessageBox.Show(oConStr);
+            
+
+        }
+    }
+}
